7e779306d2536c15cf7a4a33f064305db7e6a7f1
*q91fTG8Von <kumagikazu@gmail.com>
2022-05-09 22:12:23 +0900

    ヘッダー完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,28 @@
 $(function(){
+    ////////////////////
+    // ハンバーガーメニュー
+    ////////////////////
+    
+    // 要素を変数に格納
+    const $btn_hum = $('#hum');     // ハンバーガーメニューのボタン
+    const $sp_menu = $('#sp_menu'); // スマホメニュー
+    
+    // メニューのローディングクラスを削除
+    $sp_menu.removeClass("sp_loading");
+
+    // スマホメニュー非表示
+    $sp_menu.hide();
+
+    // ハンバーガーメニューのボタンのクラスのトグル
+    $btn_hum.click(function(){
+        $btn_hum.toggleClass("header__btn_active");
+        $sp_menu.toggleClass("sp_active");
+        if($sp_menu.hasClass("sp_active")){
+            $sp_menu.fadeIn();
+        }
+        else{
+            $sp_menu.fadeOut();
+        }
+    });
+
 });
